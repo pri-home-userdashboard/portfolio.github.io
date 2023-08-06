19f78b1ae731b40aedc86f9fa7247046c2b7c568
pri-home-userdashboard <138675437+pri-home-userdashboard@users.noreply.github.com>
2023-08-06 14:00:18 +0530

    Add files via upload

---
+++ resso/mugicLibrary.js
@@ -0,0 +1,51 @@
+export let songs = [
+    {
+        songName:"Black Dadhi| ब्लैक दाढ़ी antra_singh_priyanka",
+        singerName:"silpi raj",
+        songImage:"Black Dadhi  ब्लैक दाढ़ी",
+        songSrc:"Black Dadhi  ब्लैक दाढ़ी",
+    },
+    {
+        songName:"सेंट गमकउआ Shivani Singh Sent Gamkauwa",
+        singerName:"#Shivani Singh",
+        songImage:"sent gamkauwa",
+        songSrc:"sent gamkauwa",
+    },
+
+
+    {
+        songName:"Pawan Singh Shilpi Raj तोहार मरजी",
+        singerName:"silpi raj",
+        songImage:"Pawan Singh Shilpi Raj  तोहार मरजी",
+        songSrc:"Pawan Singh Shilpi Raj  तोहार मरजी",
+    },
+
+
+    {
+        songName:"बलमुआ मारे लागल  Balamua Maare Lagal",
+        singerName:"NeelKamal Singh",
+        songImage:"बलमुआ मारे लागल",
+        songSrc:"बलमुआ मारे लागल",
+    },
+
+
+    {
+        songName:"कमरिया डोले Kamariya Raja Ji",
+        singerName:"NeelKamal Singh #shilpi raj",
+        songImage:"कमरिया डोले",
+        songSrc:"कमरिया डोले",
+    },
+
+
+
+    {
+        songName:" हिरोइन New Song Heroin |#Bhojpuri ",
+        singerName:"NeelKamal Singh",
+        songImage:"Video  हिरोइन",
+        songSrc:"Video  हिरोइन",
+    },
+
+
+
+
+]
